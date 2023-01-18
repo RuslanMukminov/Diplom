829bfca1cb0c37672504861e2aea235a06d7a910
Ruslan Mukminov JD-CP-3 <yarus1987@gmail.com>
2023-01-18 13:52:23 +0300

    main task of the thesis

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,56 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    protected Map<String, List<PageEntry>> mapPageEntry = new HashMap<>();
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
+        File[] fileList = pdfsDir.listFiles();
+        for (File pdf : fileList) {
+            var doc = new PdfDocument(new PdfReader(pdf));
+
+            for (int i = 1; i <= doc.getNumberOfPages(); i++) {
+                PdfPage page = doc.getPage(i);
+                var text = PdfTextExtractor.getTextFromPage(page);
+                var words = text.split("\\P{IsAlphabetic}+");
+
+                Map<String, Integer> freqs = new HashMap<>();
+                for (var word : words) {
+                    if (word.isEmpty()) {
+                        continue;
+                    }
+                    word = word.toLowerCase();
+                    freqs.put(word, freqs.getOrDefault(word, 0) + 1);
+                }
+
+                for (var word : freqs.keySet()) {
+                    var pageEntry = new PageEntry(pdf.getName(), i, freqs.get(word));
+                    if (mapPageEntry.containsKey(word)) {
+                        List<PageEntry> pageEntryList = new ArrayList<>(mapPageEntry.get(word));
+                        pageEntryList.add(pageEntry);
+                        mapPageEntry.put(word, pageEntryList);
+                    } else {
+                        mapPageEntry.put(word, List.of(pageEntry));
+                    }
+                }
+            }
+        }
     }
 
     @Override
     public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+        if (mapPageEntry.containsKey(word)) {
+            List<PageEntry> pageEntryList = mapPageEntry.get(word);
+            Collections.sort(pageEntryList);
+            return pageEntryList;
+        } else {
+            return Collections.emptyList();
+        }
     }
 }
---
+++ src/main/java/Server.java
@@ -0,0 +1,57 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Server {
+    protected int port;
+    protected BooleanSearchEngine engine;
+
+    public Server(int port, BooleanSearchEngine engine) {
+        this.port = port;
+        this.engine = engine;
+    }
+
+    public void start() throws IOException {
+        System.out.println("Starting server at " + port + " port.");
+        try (var serverSocket = new ServerSocket(port)) {
+            while (true) {
+                try (
+                        Socket socket = serverSocket.accept();
+                        var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        var out = new PrintWriter(socket.getOutputStream())
+                ) {
+                    String word = in.readLine();
+                    word = word.toLowerCase();
+                    List<PageEntry> pageEntryList = engine.search(word);
+                    out.println(answerJson(pageEntryList));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
+        }
+    }
+
+    private String answerJson(List<PageEntry> pageEntryList) {
+        var builder = new GsonBuilder();
+        Gson gson = builder.create();
+        List<String> jsonList = new ArrayList<>();
+
+        if (pageEntryList.isEmpty()) {
+            return "не найдено";
+        }
+
+        for (PageEntry pageEntry : pageEntryList) {
+            jsonList.add(gson.toJson(pageEntry));
+        }
+        return jsonList.toString();
+    }
+}
