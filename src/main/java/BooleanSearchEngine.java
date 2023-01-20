066403641a2a8a48883faac5351d173bae7b15c2
Ruslan Mukminov JD-CP-3 <yarus1987@gmail.com>
2023-01-20 15:19:25 +0300

    added ability to query multiple words and added stop-words

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -3,11 +3,14 @@ import com.itextpdf.kernel.pdf.PdfPage;
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
+    private static final String FILENAME_STOPWORDS = "stop-ru.txt";
     protected Map<String, List<PageEntry>> mapPageEntry = new HashMap<>();
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
@@ -44,13 +47,89 @@ public class BooleanSearchEngine implements SearchEngine {
     }
 
     @Override
-    public List<PageEntry> search(String word) {
-        if (mapPageEntry.containsKey(word)) {
-            List<PageEntry> pageEntryList = mapPageEntry.get(word);
-            Collections.sort(pageEntryList);
-            return pageEntryList;
-        } else {
+    public List<PageEntry> search(String request) {
+        Set<String> stopWordsSet = stopWords();
+
+        List<List<PageEntry>> listsPageEntry = new ArrayList<>();
+        List<PageEntry> responseList = new ArrayList<>();
+
+        String[] words = request.split(" ");
+        for (String word : words) {
+            if (!stopWordsSet.contains(word)) {
+                if (mapPageEntry.containsKey(word)) {
+                    List<PageEntry> pageEntryList = mapPageEntry.get(word);
+//                    listsPageEntry.add(pageEntryList);
+                    if (responseList.isEmpty()) {
+                        responseList.addAll(pageEntryList);
+                        continue;
+                    }
+                    for (PageEntry pageEntry : pageEntryList) {
+                        boolean flagAddPageEntry = true;
+                        for (int i = 0; i < responseList.size(); i++) {
+                            String pdfNameFromResponse = responseList.get(i).getPdfName();
+                            int pageFromResponse = responseList.get(i).getPage();
+                            int countFromResponse = responseList.get(i).getCount();
+
+                            if (pageEntry.getPdfName().equals(pdfNameFromResponse)
+                                    && (pageEntry.getPage() == pageFromResponse)) {
+                                int countSum = countFromResponse + pageEntry.getCount();
+                                responseList.set(i, new PageEntry(pdfNameFromResponse,
+                                        pageFromResponse, countSum));
+                                flagAddPageEntry = false;
+                            }
+                        }
+                        if (flagAddPageEntry) {
+                            responseList.add(pageEntry);
+                        }
+                    }
+                }
+            }
+        }
+        if (responseList.isEmpty()) {
             return Collections.emptyList();
         }
+        //
+//        Iterator<List<PageEntry>> iterator = listsPageEntry.iterator();
+//        while (iterator.hasNext()) {
+//            List<PageEntry> pageEntryList = iterator.next();
+//            if (responseList.isEmpty()) {
+//                responseList.addAll(pageEntryList);
+//                continue;
+//            }
+//            for (PageEntry pageEntry : pageEntryList) {
+//                boolean flagAddPageEntry = true;
+//                for (int i = 0; i < responseList.size(); i++) {
+//                    String pdfNameFromResponse = responseList.get(i).getPdfName();
+//                    int pageFromResponse = responseList.get(i).getPage();
+//                    int countFromResponse = responseList.get(i).getCount();
+//
+//                    if (pageEntry.getPdfName().equals(pdfNameFromResponse)
+//                            && (pageEntry.getPage() == pageFromResponse)) {
+//                        int countSum = countFromResponse + pageEntry.getCount();
+//                        responseList.set(i, new PageEntry(pdfNameFromResponse,
+//                                pageFromResponse, countSum));
+//                        flagAddPageEntry = false;
+//                    }
+//                }
+//                if (flagAddPageEntry) {
+//                    responseList.add(pageEntry);
+//                }
+//            }
+//        }
+        Collections.sort(responseList);
+        return responseList;
+    }
+
+    private Set<String> stopWords() {
+        Set<String> stopWordsSet = new HashSet<>();
+        try (var br = new BufferedReader(new FileReader(FILENAME_STOPWORDS))) {
+            String stopWord;
+            while ((stopWord = br.readLine()) != null) {
+                stopWordsSet.add(stopWord);
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return stopWordsSet;
     }
 }
