7845e0f6c621d25246eda663bd0a2194fc0c1601
Ruslan Mukminov JD-CP-3 <yarus1987@gmail.com>
2023-01-23 15:49:09 +0300

    added new class IndexationPdf

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,47 +1,24 @@
-import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.kernel.pdf.PdfPage;
-import com.itextpdf.kernel.pdf.PdfReader;
-import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class BooleanSearchEngine implements SearchEngine {
     private static final String FILENAME_STOPWORDS = "stop-ru.txt";
-    protected Map<String, List<PageEntry>> mapPageEntry = new HashMap<>();
+    protected Map<String, List<PageEntry>> generalPageEntryMap = new HashMap<>();
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
         File[] fileList = pdfsDir.listFiles();
         for (File pdf : fileList) {
-            var doc = new PdfDocument(new PdfReader(pdf));
-
-            for (int i = 1; i <= doc.getNumberOfPages(); i++) {
-                PdfPage page = doc.getPage(i);
-                var text = PdfTextExtractor.getTextFromPage(page);
-                var words = text.split("\\P{IsAlphabetic}+");
-
-                Map<String, Integer> freqs = new HashMap<>();
-                for (var word : words) {
-                    if (word.isEmpty()) {
-                        continue;
-                    }
-                    word = word.toLowerCase();
-                    freqs.put(word, freqs.getOrDefault(word, 0) + 1);
-                }
-
-                for (var word : freqs.keySet()) {
-                    var pageEntry = new PageEntry(pdf.getName(), i, freqs.get(word));
-                    if (mapPageEntry.containsKey(word)) {
-                        List<PageEntry> pageEntryList = new ArrayList<>(mapPageEntry.get(word));
-                        pageEntryList.add(pageEntry);
-                        mapPageEntry.put(word, pageEntryList);
-                    } else {
-                        mapPageEntry.put(word, List.of(pageEntry));
-                    }
-                }
+            Map<String, List<PageEntry>> mapIndexationPdf =
+                    new IndexationPdf(pdf).getPageEntryMap();
+            for (Map.Entry<String, List<PageEntry>> entry : mapIndexationPdf.entrySet()) {
+                generalPageEntryMap.merge(entry.getKey(), entry.getValue(),
+                        (val1, val2) -> Stream.concat(val1.stream(), val2.stream())
+                                .collect(Collectors.toList()));
             }
         }
     }
@@ -49,19 +26,16 @@ public class BooleanSearchEngine implements SearchEngine {
     @Override
     public List<PageEntry> search(String request) {
         Set<String> stopWordsSet = stopWords();
-
         List<PageEntry> responseList = new ArrayList<>();
-
         String[] words = request.split(" ");
         for (String word : words) {
             if (!stopWordsSet.contains(word)) {
-                if (mapPageEntry.containsKey(word)) {
-                    List<PageEntry> pageEntryList = mapPageEntry.get(word);
+                if (generalPageEntryMap.containsKey(word)) {
                     if (responseList.isEmpty()) {
-                        responseList.addAll(pageEntryList);
+                        responseList.addAll(generalPageEntryMap.get(word));
                         continue;
                     }
-                    for (PageEntry pageEntry : pageEntryList) {
+                    for (PageEntry pageEntry : generalPageEntryMap.get(word)) {
                         boolean flagAddPageEntry = true;
                         for (int i = 0; i < responseList.size(); i++) {
                             String pdfNameFromResponse = responseList.get(i).getPdfName();
